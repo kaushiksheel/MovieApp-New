9be57331ce4e797059785b1806397b387e621011
Devil5614737 <kaushiksheel9@gmail.com>
2022-12-11 22:56:24 +0530

    ..

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,13 +8,15 @@ export default function NavComp() {
 const currentPath=params.pathname
 
   return (
-    <Navbar className="navbar" expand="lg">
+    <Navbar className="navbar" expand="lg"   style={{position:'sticky',height:'fit-content',top:0,zIndex:2}}>
       <Container>
         <Navbar.Brand style={{ color: "white", fontWeight: "bold" }} href="#">
           MoviesHub
         </Navbar.Brand>
 
-        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Toggle 
+        style={{background:'white'}}
+        aria-controls="navbarScroll" />
         <Navbar.Collapse id="navbarScroll">
           <Nav
             className="me-auto my-2 my-lg-0 d-flex gap-3"
